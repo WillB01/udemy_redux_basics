4e97a9880631ed85bd6a556cdf5a850144e0d7b6
WillB01 <william.hello@outlook.com>
2018-10-11 15:46:23 +0200

    counter state gets handled in redux

--- src/Store/reducer.js
+++ src/Store/reducer.js
@@ -3,7 +3,30 @@ const initilState = {
 }
 
 const reducer = (state = initilState, action) => {
+    if (action.type === 'INCREMENT') {
+        return {
+            counter:  state.counter + 1
+        }
+    }
+    if (action.type === 'DECREMENT') {
+        return {
+            counter: state.counter - 1
+        }
+    }
+    if (action.type === 'ADD') {
+        return {
+            counter: state.counter + 10
+        }
+    }
+    if (action.type === 'SUBSTRACT') {
+        return {
+            counter: state.counter - 8
+        }
+    }
+
+
     return state;
+
 }
 
 export default reducer;
