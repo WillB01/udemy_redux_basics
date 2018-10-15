25f575a910e6a989233f92524ec64b7176687c3e
WillB01 <william.hello@outlook.com>
2018-10-15 13:30:21 +0200

    cleaning

--- src/Store/reducers/result.js
+++ src/Store/reducers/result.js
@@ -1,32 +1,26 @@
 import * as actionTypes from '../actions/actionsTypes';
+import {updateObject} from '../utility';
+
 const initilState = {
     results: []
 }
 
+const deleteResult = (state, action) => {
+    const updatedArray = state.results.filter((result, index) => {
+        return action.id !== index;
+    });
+    return updateObject(state, {results: updatedArray});
+};
+
 const reducer = (state = initilState, action) => {
     if (action.type === actionTypes.STORE_RESULT) {
-        
-        return {
-            ...state,
-            results: state.results.concat({id: new Date(),value: action.result})
-        }
+        return updateObject(state, { results: state.results.concat({id: new Date(),value: action.result})});
     }
     if (action.type === actionTypes.DELETE_RESULT) {
-        // let id = action.id;
-        // const newArr = [...state.results];
-        // newArr.splice(id, 1);
-        const updatedArray = state.results.filter((result, index) => {
-            return action.id !== index;
-        })
-        return {
-            ...state,
-            results: updatedArray
-        }
+
+        return deleteResult(state,action);
     }
-
-
     return state;
-
 }
 
 export default reducer;
